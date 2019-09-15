34f811c1c779212e90a4b0bbf5f776bd07f99189
fortheday <fortheday@126.com>
2019-09-15 16:42:24 +0800

    修改按钮位置

--- client/src/App.vue
+++ client/src/App.vue
@@ -10,14 +10,16 @@
         active-text-color="#ffd04b">
         <el-menu-item index="/">所有数据</el-menu-item>
         <template v-if="logined">
-          <el-menu-item index="/mytargets">我的目标</el-menu-item>
-          <el-menu-item index="/mypassword">修改密码</el-menu-item>
           <template v-if="type=='老师登录'">
             <el-menu-item index="/classes">班级管理</el-menu-item>
             <el-menu-item index="/students">学生管理</el-menu-item>
             <el-menu-item index="/teachers">老师管理</el-menu-item>
             <el-menu-item index="/targets">目标管理</el-menu-item>
             <el-menu-item index="/questions">习题管理</el-menu-item>
+          </template>
+          <template v-else>
+            <el-menu-item index="/mytargets">我的目标</el-menu-item>
+            <el-menu-item index="/mypassword">修改密码</el-menu-item>
           </template>
           <el-menu-item index="/logout" @click="setLogout">{{username}} 退出</el-menu-item>
         </template>
