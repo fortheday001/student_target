edadfd0b998aa4fc44fd9a233f2dda8631f9d6d9
fortheday <fortheday@126.com>
2019-09-18 11:57:45 +0800

    添加配置文件

---
+++ server/config-example.js
@@ -0,0 +1,10 @@
+module.exports = {
+    db: {
+        connectionLimit : 20,
+        host            : '127.0.0.1',
+        user            : 'root',
+        password        : '123123',
+        database        : 'student_target',
+        charset         : 'utf8mb4'
+    }
+}
